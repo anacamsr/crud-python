075e6ca4cfdf45a2bd77d5ce7ff5f8f3b541a876
anacamsr <anacamsr@gmail.com>
2024-01-09 21:02:04 -0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app.forms import CarrosForm
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def form(request):
+    data = {}
+    data['form'] = CarrosForm()
+    return render(request, 'form.html', data)
